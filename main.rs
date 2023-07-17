6e1e4e983bfdc4c2d34d506edf71d92f5dc57bb8
conrizzo <conrizzo@gmail.com>
2023-07-17 12:29:08 +0200

    start basic class, build a grid, maybe make a game in rust?

--- main.rs
+++ main.rs
@@ -2,20 +2,46 @@
 
 // function to add two numbers
 fn add() {
-
-    let total_amount_paid = 115+35+25;
-    let total_amount_due: i32 = 35+35+31;
-    //let a = 32 + 32 + 32;
-    //let b = 10;
+    let total_amount_paid = 115+25+35;
+    let total_amount_due: i32 = 32+32+32;
 
     let sum = total_amount_paid - total_amount_due;
-
-    println!("test addition function = {}", sum);
+    println!("test addition function = {}", sum);   
 }
 
 
 
+struct Item {
+    name: String,    
+    position: (i32, i32),
+}
+
+fn make_grid(){
+    let rows = 20;
+    let cols = 20;
+
+    // create a 2D grid of integers with a size of 3x3
+    let grid = vec![vec![0; cols]; rows];
+
+    // print the grid
+    for row in &grid {
+        println!("{:?}", row);    }
+
+}
+
+fn make_item(){
+    let item = Item {
+        name: String::from("First Item"),
+        position: (2, 2),
+    };
+    println!("item name = {}", item.name);
+    println!("item position = {:?}", item.position);
+}
+
+
 fn main() {
     println!("main function!");
-    add()
+    add();
+    make_grid();
+    make_item();
 }
